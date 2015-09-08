1e4841927548eb6c1c51b3bf8a48d343757c585d
LC <lrc3233@gmail.com>
2015-09-07 23:02:53 -0700

    add whitenoise? and placeholder static directory...

--- deploy_dashboard/deploy_dashboard/wsgi.py
+++ deploy_dashboard/deploy_dashboard/wsgi.py
@@ -11,7 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deploy_dashboard.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deploy_dashboard.deploy_dashboard.settings")
 
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
