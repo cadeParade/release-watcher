8106df8190c71a0e7fbf8f4c192ac394db5a162f
Raja Gangopadhya <rajag@altschool.com>
2015-10-07 13:42:11 -0700

    Add PM manager field with default, along with migration.

--- deploy_dashboard/deploys/models.py
+++ deploy_dashboard/deploys/models.py
@@ -4,7 +4,8 @@ class Release(models.Model):
     version = models.CharField(max_length=10)
     code_freeze_date = models.DateTimeField()
     production_release_date = models.DateTimeField()
-    release_manager = models.CharField(max_length=200)
+    dev_release_manager = models.CharField(max_length=200)
+    pm_release_manager = models.CharField(max_length=200, default='')
 
     def __unicode__(self):
       return "Release %s" % self.version
